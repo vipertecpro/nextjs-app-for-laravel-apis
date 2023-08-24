425cb9c8835684f24f596ab903347714d1406a90
vipertecpro <vipertecpro@gmail.com>
2023-08-24 12:27:03 +0530

    Books Listing Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     content: ['./src/**/*.js'],
-    darkMode: 'media',
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
