50de2e20b65ac1b336d6c52690022565adc44ec8
vipertecpro <vipertecpro@gmail.com>
2023-08-25 22:52:55 +0530

    Searching, Pagination and Mapping is done

---
+++ src/app/(front_pages)/book/[...slug]/page.js
@@ -0,0 +1,8 @@
+'use client'
+export default function SingleBook({ params }) {
+    return (
+        <>
+            <h1>Post: {params.slug}</h1>
+        </>
+    )
+}
--- src/app/(front_pages)/page.js
+++ src/app/(front_pages)/page.js
@@ -2,53 +2,49 @@
 
 import useSWR from 'swr'
 import {
-    Avatar,
     Card,
     CardBody,
     CardFooter,
     CardHeader,
-    Tooltip,
     Typography,
 } from '@material-tailwind/react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useEffect, useRef, useState } from 'react'
 
 async function fetcher(url) {
     const res = await fetch(url)
     if (!res.ok) throw new Error('Failed to fetch data')
     return res.json()
 }
-export default function HomePage() {
-    const { data: getBookCategories, error } = useSWR(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/client/books`,
+export function RenderBooks({ searchTerm, currentPage, setCurrentPage }) {
+    const { data, error } = useSWR(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/client/books?page=${currentPage}&search=${searchTerm}`,
         fetcher,
         {
             revalidateOnFocus: false,
             refreshInterval: 10000,
         },
     )
+
+    if (error) {
+        return <>Failed to load</>
+    }
+    if (!data) {
+        return <>Loading...</>
+    }
+
     return (
         <>
-            <div className={'p-5'}>
-                <div className="px-5 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-5 lg:px-5 lg:py-10">
-                    <div className="relative">
-                        <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-5">
-                            <input
-                                placeholder="Search . . . "
-                                required
-                                type="text"
-                                className="flex-grow w-full h-16 text-4xl px-4 mb-3 font-bold text-gray-900 transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-gray-300 focus:border-gray-200 focus:outline-none focus:shadow-outline"
-                            />
-                        </form>
-                    </div>
-                </div>
-                <div className={` border-4 border-gray-400  p-5 rounded-2xl`}>
-                    <div
-                        className={
-                            'grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5'
-                        }>
-                        <Link href={''} className={'w-100 inline'}>
+            <div className={`border-4 border-gray-400 p-5 rounded-2xl`}>
+                <div
+                    className={
+                        'grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5'
+                    }>
+                    {data.pageData.data.map(book => (
+                        <Link href={`/book/${book.slug}`} key={book.id}>
                             <Card className="max-w-100 md:max-w-[18rem] overflow-hidden">
+                                {/* Details for each book */}
                                 <CardHeader
                                     floated={false}
                                     shadow={false}
@@ -59,78 +55,90 @@ export default function HomePage() {
                                         className={
                                             'h-auto max-w-full object-cover'
                                         }
-                                        src={'/placeholder.avif'}
+                                        src={book.image || '/placeholder.avif'}
                                         alt="ui/ux review check"
                                     />
                                 </CardHeader>
                                 <CardBody className={'p-2'}>
                                     <Typography variant="h5" color="blue-gray">
-                                        Book Name
+                                        {book.name}
                                     </Typography>
                                     <Typography
                                         variant="small"
                                         color="blue-gray">
-                                        Author Name
-                                    </Typography>
-                                    <Typography
-                                        variant="paragraph"
-                                        color="gray"
-                                        className="mt-2 font-normal">
-                                        Because it&apos;s about motivating the
-                                        doers. Because I&apos;m here to follow
-                                        my dreams and inspire others.
+                                        {book.author}
                                     </Typography>
                                 </CardBody>
                                 <CardFooter className="p-2 flex items-center justify-between">
                                     <Typography className="font-normal">
-                                        TR : 0
+                                        TR: {book.book_reviews_count}
                                     </Typography>
                                     <Typography className="font-normal">
-                                        AR : 0
+                                        AR: {book.book_reviews_avg_rating}
                                     </Typography>
                                 </CardFooter>
                             </Card>
                         </Link>
+                    ))}
+                </div>
+            </div>
+            <div className="flex items-center justify-center gap-3 mx-auto p-20">
+                {/* Pagination code here */}
+                <Link
+                    href={'/'}
+                    disabled={currentPage === 1}
+                    onClick={() => setCurrentPage(currentPage - 1)}>
+                    Previous Page
+                </Link>
+                <p className="text-2xl font-bold text-gray-900">
+                    {currentPage}
+                    <span className="mx-0.25">/</span>
+                    {data.pageData.last_page}
+                </p>
+                <Link
+                    href={'/'}
+                    disabled={currentPage === data.pageData.last_page}
+                    onClick={() => setCurrentPage(currentPage + 1)}>
+                    Next Page
+                </Link>
+            </div>
+        </>
+    )
+}
+export default function HomePage() {
+    const [searchTerm, setSearchTerm] = useState('')
+    const [currentPage, setCurrentPage] = useState(1)
+    const searchInputRef = useRef(null)
+
+    useEffect(() => {
+        if (searchInputRef.current) {
+            searchInputRef.current.focus()
+        }
+    }, [searchTerm])
+
+    return (
+        <>
+            <div className={'p-5'}>
+                <div className="px-5 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-5 lg:px-5 lg:py-10">
+                    <div className="relative">
+                        <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-5">
+                            <input
+                                ref={searchInputRef}
+                                placeholder="Search . . . "
+                                required
+                                type="text"
+                                className="flex-grow w-full h-16 text-4xl px-4 mb-3 font-bold text-gray-900 transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-gray-300 focus:border-gray-200 focus:outline-none focus:shadow-outline"
+                                value={searchTerm}
+                                onChange={e => setSearchTerm(e.target.value)}
+                            />
+                        </form>
                     </div>
                 </div>
-
-                <div className="flex items-center justify-center gap-3 mx-auto p-20">
-                    <Link
-                        href={'/'}
-                        className="inline-flex h-10 w-10 items-center justify-center rounded border border-gray-800 bg-white text-gray-900 rtl:rotate-180">
-                        <span className="sr-only">Next Page</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-18 w-18"
-                            viewBox="0 0 20 20"
-                            fill="currentColor">
-                            <path
-                                fillRule="evenodd"
-                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </Link>
-                    <p className="text-2xl font-bold text-gray-900">
-                        0<span className="mx-0.25">/</span>12
-                    </p>
-                    <Link
-                        href={'/'}
-                        className="inline-flex h-10 w-10 items-center justify-center rounded border border-gray-800 bg-white text-gray-900 rtl:rotate-180">
-                        <span className="sr-only">Next Page</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-18 w-18"
-                            viewBox="0 0 20 20"
-                            fill="currentColor">
-                            <path
-                                fillRule="evenodd"
-                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </Link>
-                </div>
+                <RenderBooks
+                    searchTerm={searchTerm}
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                />
             </div>
         </>
     )
