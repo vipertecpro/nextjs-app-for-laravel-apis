c6afb13e3b489fec0ddfb8b5577c7930c67a43c4
vipertecpro <vipertecpro@gmail.com>
2023-08-25 23:56:08 +0530

    Single Book Review Page is Added

--- src/app/(dashboard_pages)/dashboard/page.js
+++ src/app/(dashboard_pages)/dashboard/page.js
@@ -15,21 +15,8 @@ import {
 
 const navigation = [
     { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    {
-        name: 'Documents',
-        href: '#',
-        icon: DocumentDuplicateIcon,
-        current: false,
-    },
-    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
-]
-const teams = [
-    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
-    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
-    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+    { name: 'Books', href: '#', icon: UsersIcon, current: false },
+    { name: 'Reviews', href: '#', icon: FolderIcon, current: false },
 ]
 
 function classNames(...classes) {
@@ -152,40 +139,6 @@ export default function Example() {
                                                         )}
                                                     </ul>
                                                 </li>
-                                                <li>
-                                                    <div className="text-xs font-semibold leading-6 text-indigo-200">
-                                                        Your teams
-                                                    </div>
-                                                    <ul
-                                                        role="list"
-                                                        className="-mx-2 mt-2 space-y-1">
-                                                        {teams.map(team => (
-                                                            <li key={team.name}>
-                                                                <a
-                                                                    href={
-                                                                        team.href
-                                                                    }
-                                                                    className={classNames(
-                                                                        team.current
-                                                                            ? 'bg-indigo-700 text-white'
-                                                                            : 'text-indigo-200 hover:text-white hover:bg-indigo-700',
-                                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
-                                                                    )}>
-                                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                                                        {
-                                                                            team.initial
-                                                                        }
-                                                                    </span>
-                                                                    <span className="truncate">
-                                                                        {
-                                                                            team.name
-                                                                        }
-                                                                    </span>
-                                                                </a>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </li>
                                             </ul>
                                         </nav>
                                     </div>
@@ -232,34 +185,6 @@ export default function Example() {
                                                         aria-hidden="true"
                                                     />
                                                     {item.name}
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </li>
-                                <li>
-                                    <div className="text-xs font-semibold leading-6 text-indigo-200">
-                                        Your teams
-                                    </div>
-                                    <ul
-                                        role="list"
-                                        className="-mx-2 mt-2 space-y-1">
-                                        {teams.map(team => (
-                                            <li key={team.name}>
-                                                <a
-                                                    href={team.href}
-                                                    className={classNames(
-                                                        team.current
-                                                            ? 'bg-indigo-700 text-white'
-                                                            : 'text-indigo-200 hover:text-white hover:bg-indigo-700',
-                                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
-                                                    )}>
-                                                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                                        {team.initial}
-                                                    </span>
-                                                    <span className="truncate">
-                                                        {team.name}
-                                                    </span>
                                                 </a>
                                             </li>
                                         ))}
--- src/app/(front_pages)/book/[...slug]/page.js
+++ src/app/(front_pages)/book/[...slug]/page.js
@@ -1,8 +1,94 @@
 'use client'
+import useSWR from 'swr'
+import Image from 'next/image'
+async function fetcher(url) {
+    const res = await fetch(url)
+    if (!res.ok) throw new Error('Failed to fetch data')
+    return res.json()
+}
 export default function SingleBook({ params }) {
+    const { data, error } = useSWR(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/client/book/${params.slug}`,
+        fetcher,
+        {
+            revalidateOnFocus: false,
+            refreshInterval: 10000,
+        },
+    )
+    if (error) {
+        return <>Failed to load</>
+    }
+    if (!data) {
+        return <>Loading...</>
+    }
     return (
         <>
-            <h1>Post: {params.slug}</h1>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div className="grid gap-10 row-gap-8 lg:grid-cols-5">
+                    <div className="lg:col-span-2">
+                        <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+                            {data.pageData.book.created_at}
+                        </p>
+                        <div className="mb-3">
+                            <div className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-400">
+                                <p className="font-sans text-xl font-extrabold leading-none tracking-tight lg:text-4xl xl:text-5xl">
+                                    {data.pageData.book.title}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="mr-3">
+                                <img
+                                    alt="avatar"
+                                    src="/user.png"
+                                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                                />
+                            </div>
+                            <div>
+                                <div className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400">
+                                    {data.pageData.book.author}
+                                </div>
+                                <p className="text-sm font-medium leading-4 text-gray-600">
+                                    Author
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col space-y-8 lg:col-span-3">
+                        {data.pageData.bookReviews.data.map(review => {
+                            return (
+                                <>
+                                    <div key={review.id}>
+                                        <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+                                            {review.created_at}
+                                        </p>
+                                        <p className="mb-4 text-base text-gray-700 md:text-lg">
+                                            {review.content}
+                                        </p>
+                                        <div className="flex items-center">
+                                            <div className="mr-3">
+                                                <Image
+                                                    width={100}
+                                                    height={100}
+                                                    alt="avatar"
+                                                    src="/user.png"
+                                                    className="object-cover w-10 h-10 rounded-full shadow-sm"
+                                                />
+                                            </div>
+                                            <div>
+                                                <div className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400">
+                                                    Posted By :{' '}
+                                                    {review.created_by.name}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
